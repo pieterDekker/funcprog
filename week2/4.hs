443b06df8c463880297ded882305712341b44855
pieterDekker <pieter.dekker20@gmail.com>
2016-10-06 12:22:01 +0200

    localstuff

--- week2/4.hs
+++ week2/4.hs
@@ -1,13 +1,17 @@
+import Data.List
+
 removables :: [Integer]
-removables = [i + j + (2 * i * j) | j <- [1..], i <- [1..j]]
+removables = [i + j + (2 * i * j) | i <- [1..], j <- [1..], i <= j]
+
+isRemovable2 :: Integer -> Bool
+isRemovable2 n = elem n [i + j + (2 * i * j) | let n'=fromIntegral n,
+                                               i<-[1..floor (sqrt (n' / 2))],
+                                               let i' = fromIntegral i,
+                                               j<-[i..floor( (n'-i')/(2*i'+1))]]
 
 isRemovable :: Integer -> Bool
-isRemovable n = n == head ( dropWhile (<n) removables )
+isRemovable n = n == head ( dropWhile (<2*n) removables )
 
 primes :: [Integer]
-primes = 2 : [(2 * n) + 1 | n <- [1..], not (isRemovable n)]
+primes = 2 : [(2 * n) + 1 | n <- [1..], not (isRemovable2 n)]
 
---someRemovables = [4,7,10,13,16,19,22,25,28,31,12,17,22,27,32,37,42,47,52,24,31,38,45,52,59,66,73,40,49,58,67,76,85,94,60,71,82,93,104,115,84,97,110,123,136,112,127,142,157,144,161,178,180,199,220]
-
-slc :: Int -> [(Int, Int)]
-slc n = [(i, j) | i <- [1..n], j <- [i..n]]
--- week2/psievewrapper.hs
+++ week2/psievewrapper.hs
@@ -1,11 +1,16 @@
-removables :: [Integer]
-removables = [i + j + (2 * i * j) | j <- [1..], i <- [1..j]]
+import Data.List
+
+removList :: [Integer]
+removList  = 4:7:removables
 
 isRemovable :: Integer -> Bool
-isRemovable n = n == head ( dropWhile (<n) removables )
-
-primes :: [Integer]
+isRemovable n = elem n [i + j + (2 * i * j) | let n'=fromIntegral n,
+                                               i<-[1..floor (sqrt (n' / 2))],
+                                               let i' = fromIntegral i,
+                                               j<-[i..floor( (n'-i')/(2*i'+1))]]
+                                               
 primes = 2 : [(2 * n) + 1 | n <- [1..], not (isRemovable n)]
+
 
 -- Do not change the following wrapper code
 wrapper :: String -> [Integer]
